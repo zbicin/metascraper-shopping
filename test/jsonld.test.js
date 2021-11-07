5dd67ad62757eddf0c82a5b9695cfce5e7c090e7
Krzysztof Zbicinski <zbicin@users.noreply.github.com>
2021-11-07 20:26:37 +0100

    add test against multiple ldjson

--- test/jsonld.test.js
+++ test/jsonld.test.js
@@ -15,3 +15,44 @@ test("jsonld test", async () => {
   expect(metadata.name).toBe("Calacatta Nuvo");
   expect(metadata.retailer).toBe("Caesarstone US");
 }, 10000);
+
+test("do not stop when first jsonld yields no results", async () => {
+  const url =
+    "https://localhost";
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@graph": [
+        {
+          "@type": "Organization"
+        }
+      ]
+    }
+    </script>
+    <script type="application/ld+json">
+    {
+      "@context": "http://schema.org/",
+      "@type": "Product",
+      "offers": {
+        "@type": "Offer",
+        "price": "3099.00"
+      }
+    }
+    </script>
+    
+  </head>
+  <body>
+  </body>
+  </html>`;
+
+  const metadata = await metascraper({ html, url });
+  expect(metadata.price).toBe(3099);
+});
